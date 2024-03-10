aa5d8eec4da044f049b5f020342dea12b204ce4c
eli-ghub <128964493+eli-ghub@users.noreply.github.com>
2024-03-10 16:33:30 +0200

    Update non_sklearn_models.py

--- non_sklearn_models.py
+++ non_sklearn_models.py
@@ -1,1 +1,7 @@
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
+from xgboost import XGBClassifier
 
+model = CatBoostClassifier(random_seed=42,auto_class_weights=None)
+model = LGBMClassifier(random_state=42)
+model = XGBClassifier(random_state=42)
