051663ca5b69db47c90b8a8e7a4015f58dbade82
David Ung <david.unga@gmail.com>
2024-03-14 09:56:03 +0200

    add LGBM and CatBoost

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -6,12 +6,10 @@ from config import get_config, get_config_id
 import pandas as pd
 from typing import Dict, List, Tuple
 import xgboost as xgb
+import lightgbm as lgb
+import catboost as catb
 import numpy as np
 import cv_result_manager
-
-
-def _reduce_grid(grid: Dict) -> Dict:
-    return {k: [v[0], v[-1]] if len(v) > 1 else v for k, v in grid.items()}
 
 
 grids = {
@@ -29,8 +27,36 @@ grids = {
             'reg_alpha': [1e-5, 1e-3, 1e-2, 1, 1e2],
             'reg_lambda': [1e-5, 1e-3, 1e-2, 1, 1e2],
         }
+    },
+    'LGB': {
+        'estimator': lgb.LGBMClassifier,
+        'base_grid': {
+            'max_depth': np.arange(3, 5),
+            'learning_rate': np.linspace(0.01, 0.1, 5)
+        },
+        'fine_grid': {
+            'min_child_weight': [1, 3, 5, 7, 10],
+            'subsample': [.6, .8, 1.],
+        }
+    },
+    'CATB': {
+        'estimator': catb.CatBoostClassifier,
+        'base_grid': {
+            'max_depth': np.arange(3, 5),
+            'learning_rate': np.linspace(0.01, 0.1, 5)
+        },
+        'fine_grid': {
+            'bagging_temperature': [0, .5, 1, 5],
+        }
     }
 }
+
+
+def _get_best_iteration(clf):
+    for attr in ['best_iteration', 'best_iteration_', '_best_iteration']:
+        if hasattr(clf, attr):
+            return getattr(clf, attr)
+    assert AttributeError()
 
 
 def tune(config: Dict):
@@ -54,7 +80,7 @@ def tune(config: Dict):
         base_grid = grids[model_name]['base_grid']
         fine_grid = grids[model_name]['fine_grid']
 
-        DEV = False
+        DEV = True
         if DEV:
             # smaller grid for dev/debug..
             base_grid = _reduce_grid(base_grid)
@@ -68,8 +94,7 @@ def tune(config: Dict):
         cv = GridSearchCV(model, param_grid=base_grid, **cv_args)
         cv.fit(*Xy_base_train, eval_set=[Xy_base_val])
         params = cv.best_params_
-        params['n_estimators'] = cv.best_estimator_.best_iteration
-
+        params['n_estimators'] = _get_best_iteration(cv.best_estimator_)
         print(model_name + ":")
         print(" Best base params:", params)
         print(" Score:", cv.best_score_)
@@ -87,6 +112,10 @@ def tune(config: Dict):
         results_df = cv_result_manager.make_dataframe(config, model_name, random_state, cv.cv_results_)
         cv_result_manager.display(results_df)
         cv_result_manager.save(results_df)
+
+
+def _reduce_grid(grid: Dict) -> Dict:
+    return {k: [v[0], v[-1]] if len(v) > 1 else v for k, v in grid.items()}
 
 
 if __name__ == "__main__":
