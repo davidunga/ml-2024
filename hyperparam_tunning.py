3cf5c8b4cd4cdfab20fc00e474ad6fc9a12940b8
David Ung <david.unga@gmail.com>
2024-03-14 09:27:27 +0200

    hyeperparam tunning - general grid spec

--- hyperparam_tunning.py
+++ hyperparam_tunning.py
@@ -5,32 +5,35 @@ from data import load_data, build_pipeline, DataSplitter
 from config import get_config, get_config_id
 import pandas as pd
 from typing import Dict, List, Tuple
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
-from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
-from sklearn.model_selection import train_test_split
 import numpy as np
+import cv_result_manager
 
 
 def _reduce_grid(grid: Dict) -> Dict:
     return {k: [v[0], v[-1]] if len(v) > 1 else v for k, v in grid.items()}
 
 
-def tune_xgboost(config: Dict):
-
-    base_grid = {
-        'max_depth': np.arange(3, 5),
-        'learning_rate': np.linspace(0.01, 0.1, 5)
+grids = {
+    'XGB': {
+        'estimator': xgb.XGBClassifier,
+        'base_grid': {
+            'max_depth': np.arange(3, 5),
+            'learning_rate': np.linspace(0.01, 0.1, 5)
+        },
+        'fine_grid': {
+            'min_child_weight': [1, 3, 5, 7, 10],
+            'gamma': [0, .1, .2, .3, .4],
+            'subsample': [.6, .8, 1.],
+            'colsample_bytree': [.6, .8, 1.],
+            'reg_alpha': [1e-5, 1e-3, 1e-2, 1, 1e2],
+            'reg_lambda': [1e-5, 1e-3, 1e-2, 1, 1e2],
+        }
     }
+}
 
-    finetune_grid = {
-        'min_child_weight': [1, 3, 5, 7, 10],
-        'gamma': [0, .1, .2, .3, .4],
-        'subsample': [.6, .8, 1.],
-        'colsample_bytree': [.6, .8, 1.],
-        'reg_alpha': [1e-5, 1e-3, 1e-2, 1, 1e2],
-        'reg_lambda': [1e-5, 1e-3, 1e-2, 1, 1e2],
-    }
+
+def tune(config: Dict):
 
     cv_args = {'cv': config['cv.n_folds'], 'scoring': config['cv.scores'],
                'verbose': 0, 'refit': config['cv.main_score'], 'return_train_score': True, 'n_jobs': -1}
@@ -45,27 +48,46 @@ def tune_xgboost(config: Dict):
     Xy_train, Xy_test = data_splitter.split(Xy, test_size=config['data.test_size'])
     Xy_base_train, Xy_base_val = data_splitter.split(Xy_train, test_size=config['cv.base.val_size'])
 
-    # find best base params:
-    print("Searching for best base params...")
+    for model_name in grids:
 
-    model = xgb.XGBClassifier(early_stopping_rounds=config['cv.base.early_stopping_rounds'], random_state=random_state)
-    cv = GridSearchCV(model, param_grid=base_grid, **cv_args)
-    cv.fit(*Xy_base_train, eval_set=[Xy_base_val])
-    params = cv.best_params_
-    params['n_estimators'] = cv.best_estimator_.best_iteration
+        estimator_class = grids[model_name]['estimator']
+        base_grid = grids[model_name]['base_grid']
+        fine_grid = grids[model_name]['fine_grid']
 
-    print("Best base params:", params)
-    print("Score:", cv.best_score_)
+        DEV = False
+        if DEV:
+            # smaller grid for dev/debug..
+            base_grid = _reduce_grid(base_grid)
+            fine_grid = _reduce_grid(fine_grid)
 
-    # fine tune:
-    print("Fine tuning...")
-    model = xgb.XGBClassifier(**params, random_state=random_state)
-    cv = GridSearchCV(model, param_grid=finetune_grid, **cv_args)
-    cv.fit(*Xy_train)
+        model = estimator_class(early_stopping_rounds=config['cv.base.early_stopping_rounds'],
+                                random_state=random_state)
 
-    print("Best fine-tuned params:", params)
-    print("Score:", cv.best_score_)
+        # find best base params:
+        print(model_name, "- Searching for best base params...")
+        cv = GridSearchCV(model, param_grid=base_grid, **cv_args)
+        cv.fit(*Xy_base_train, eval_set=[Xy_base_val])
+        params = cv.best_params_
+        params['n_estimators'] = cv.best_estimator_.best_iteration
+
+        print(model_name + ":")
+        print(" Best base params:", params)
+        print(" Score:", cv.best_score_)
+
+        # fine tune:
+        print(model_name, "- Fine tuning...")
+        model = estimator_class(**params, random_state=random_state)
+        cv = GridSearchCV(model, param_grid=fine_grid, **cv_args)
+        cv.fit(*Xy_train)
+
+        print(model_name + ":")
+        print(" Best fine-tuned params:", params)
+        print(" Score:", cv.best_score_)
+
+        results_df = cv_result_manager.make_dataframe(config, model_name, random_state, cv.cv_results_)
+        cv_result_manager.display(results_df)
+        cv_result_manager.save(results_df)
 
 
 if __name__ == "__main__":
-    tune_xgboost(config=get_config())
+    tune(config=get_config())
